260fb8b9cf40e67587748a503dfbca00d5025c5c
JK7 <99865958+jeffykings@users.noreply.github.com>
2024-09-25 03:29:18 +0100

    Update raycasting.c

--- raycasting.c
+++ raycasting.c
@@ -1,102 +1,36 @@
 #include "main.h"
 
 /**
- * render_walls - Render the 3D walls using raycasting, now with textures
+ * cast_rays - Perform raycasting
+ * @game: Pointer to the GameState structure
  */
-void render_walls(void)
+void cast_rays(GameState *game)
 {
-    for (int x = 0; x < SCREEN_WIDTH; x++)
-    {
-        double camera_x = 2 * x / (double)SCREEN_WIDTH - 1;
-        double ray_dir_x = player.dir_x + player.plane_x * camera_x;
-        double ray_dir_y = player.dir_y + player.plane_y * camera_x;
+    /* Implement raycasting logic here */
+    /* This function will calculate wall distances for rendering */
+}
 
-        int map_x = (int)player.x;
-        int map_y = (int)player.y;
+/**
+ * draw_walls - Draw walls based on raycasting results
+ * @game: Pointer to the GameState structure
+ */
+void draw_walls(GameState *game)
+{
+    /* Implement wall drawing logic here */
+    /* This function will use the results from cast_rays to render walls */
+}
 
-        double side_dist_x, side_dist_y;
+/**
+ * draw_floor_ceiling - Draw floor and ceiling
+ * @game: Pointer to the GameState structure
+ */
+void draw_floor_ceiling(GameState *game)
+{
+    SDL_SetRenderDrawColor(game->renderer, 100, 100, 100, 255);
+    SDL_Rect floor = {0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2};
+    SDL_RenderFillRect(game->renderer, &floor);
 
-        double delta_dist_x = fabs(1 / ray_dir_x);
-        double delta_dist_y = fabs(1 / ray_dir_y);
-        double perp_wall_dist;
-
-        int step_x, step_y;
-        int hit = 0;
-        int side;
-
-        if (ray_dir_x < 0)
-        {
-            step_x = -1;
-            side_dist_x = (player.x - map_x) * delta_dist_x;
-        }
-        else
-        {
-            step_x = 1;
-            side_dist_x = (map_x + 1.0 - player.x) * delta_dist_x;
-        }
-
-        if (ray_dir_y < 0)
-        {
-            step_y = -1;
-            side_dist_y = (player.y - map_y) * delta_dist_y;
-        }
-        else
-        {
-            step_y = 1;
-            side_dist_y = (map_y + 1.0 - player.y) * delta_dist_y;
-        }
-
-        while (hit == 0)
-        {
-            if (side_dist_x < side_dist_y)
-            {
-                side_dist_x += delta_dist_x;
-                map_x += step_x;
-                side = 0;
-            }
-            else
-            {
-                side_dist_y += delta_dist_y;
-                map_y += step_y;
-                side = 1;
-            }
-            if (map[map_x][map_y] > 0)
-                hit = 1;
-        }
-
-        if (side == 0)
-            perp_wall_dist = (map_x - player.x + (1 - step_x) / 2) / ray_dir_x;
-        else
-            perp_wall_dist = (map_y - player.y + (1 - step_y) / 2) / ray_dir_y;
-
-        int line_height = (int)(SCREEN_HEIGHT / perp_wall_dist);
-
-        int draw_start = -line_height / 2 + SCREEN_HEIGHT / 2;
-        if (draw_start < 0)
-            draw_start = 0;
-        int draw_end = line_height / 2 + SCREEN_HEIGHT / 2;
-        if (draw_end >= SCREEN_HEIGHT)
-            draw_end = SCREEN_HEIGHT - 1;
-
-        /* Select texture based on wall orientation */
-        SDL_Texture *wall_texture;
-        if (side == 1)
-        {
-            if (ray_dir_y > 0)
-                wall_texture = wall_textures[1]; /* South */
-            else
-                wall_texture = wall_textures[0]; /* North */
-        }
-        else
-        {
-            if (ray_dir_x > 0)
-                wall_texture = wall_textures[3]; /* West */
-            else
-                wall_texture = wall_textures[2]; /* East */
-        }
-
-        /* Render the texture on the wall */
-        SDL_Rect rect = {x, draw_start, 1, draw_end - draw_start};
-        SDL_RenderCopy(renderer, wall_texture, NULL, &rect);
-    }
+    SDL_SetRenderDrawColor(game->renderer, 200, 200, 200, 255);
+    SDL_Rect ceiling = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT / 2};
+    SDL_RenderFillRect(game->renderer, &ceiling);
 }
