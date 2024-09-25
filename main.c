d37537910cea2d65f97079cf58eec5d611f25643
JK7 <99865958+jeffykings@users.noreply.github.com>
2024-09-25 03:25:44 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,47 +1,45 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-#include <SDL2/SDL.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-/* Screen dimensions */
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
-#define MAP_WIDTH 24
-#define MAP_HEIGHT 24
-#define MOVE_SPEED 0.1
-#define ROT_SPEED 0.05
-
-/* Player struct */
-typedef struct Player
+/**
+ * main - Entry point for the maze game
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ * Return: 0 on success, 1 on failure
+ */
+int main(int argc, char *argv[])
 {
-    float x, y;
-    float dir_x, dir_y;
-    float plane_x, plane_y;
-} Player;
+    GameState game;
+    bool quit = false;
 
-/* Function prototypes */
-int init_sdl(void);
-void cleanup_sdl(void);
-void handle_input(void);
-void render_walls(void);
-int load_map(const char *filename);
-int load_textures(void);
-void cleanup_textures(void);
-int check_collision(float new_x, float new_y);
-void move_forward(void);
-void move_backward(void);
-void rotate_left(void);
-void rotate_right(void);
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <map_file>\n", argv[0]);
+        return 1;
+    }
 
-/* External declarations */
-extern SDL_Window *window;
-extern SDL_Renderer *renderer;
-extern int quit_game;
-extern int map[MAP_WIDTH][MAP_HEIGHT];
-extern Player player;
-extern SDL_Texture *wall_textures[4];
+    if (!init_sdl(&game))
+    {
+        fprintf(stderr, "Failed to initialize SDL\n");
+        return 1;
+    }
 
-#endif /* MAIN_H */
+    if (!load_map(&game, argv[1]))
+    {
+        fprintf(stderr, "Failed to load map\n");
+        cleanup(&game);
+        return 1;
+    }
+
+    while (!quit)
+    {
+        handle_events(&game, &quit);
+        cast_rays(&game);
+        draw_floor_ceiling(&game);
+        draw_walls(&game);
+        draw_map(&game);
+        SDL_RenderPresent(game.renderer);
+    }
+
+    cleanup(&game);
+    return 0;
+}
