b6ee6a596ace338b8d7f3f3baf19455bb0d9479c
JK7 <99865958+jeffykings@users.noreply.github.com>
2024-09-25 17:31:34 +0100

    Update and rename input.c to cleanup.c

---
+++ cleanup.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * cleanup - Free resources and quit SDL
+ * @game: Pointer to the GameState structure
+ */
+void cleanup(GameState *game)
+{
+    SDL_DestroyRenderer(game->renderer);
+    SDL_DestroyWindow(game->window);
+    SDL_Quit();
+}
