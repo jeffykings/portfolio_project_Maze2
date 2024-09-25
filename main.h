9943870f12e2529332a08462f464190f5bab5cc4
JK7 <99865958+jeffykings@users.noreply.github.com>
2024-09-25 18:16:29 +0100

    Update main.h 3

--- main.h
+++ main.h
@@ -10,14 +10,19 @@
 #define MAP_WIDTH 24
 #define MAP_HEIGHT 24
 #define TILE_SIZE 64
+#define MAX_ENEMIES 10
 
-/** Structure to represent a 2D vector */
 typedef struct {
     float x;
     float y;
 } Vector2;
 
-/** Structure to hold game state */
+typedef struct {
+    Vector2 position;
+    float angle;
+    bool is_active;
+} Enemy;
+
 typedef struct {
     SDL_Window *window;
     SDL_Renderer *renderer;
@@ -25,6 +30,10 @@ typedef struct {
     Vector2 player_pos;
     float player_angle;
     bool show_map;
+    bool is_raining;
+    int current_weapon;
+    Enemy enemies[MAX_ENEMIES];
+    int num_enemies;
 } GameState;
 
 /** Function prototypes */
@@ -38,6 +47,11 @@ void unload_textures(void);
 SDL_Color get_texture_color(int texture_id, int x, int y);
 bool handle_collisions(GameState *game, float new_x, float new_y);
 void draw_map(GameState *game);
+void init_enemies(GameState *game);
+void update_enemies(GameState *game);
+void render_enemies(GameState *game);
+void render_weapon(GameState *game);
+void render_rain(GameState *game);
 
 /** Global variables */
 extern int worldMap[MAP_WIDTH][MAP_HEIGHT];
